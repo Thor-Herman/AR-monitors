d6d898af8da2046920cc92bac584675cfac216bb
Thor-Herman <44784559+Thor-Herman@users.noreply.github.com>
2021-12-29 15:59:52 +0100

    Make MenuButtons use serialiedfield instead of gameobject.find

--- Assets/CanvasFiles/MenuButtonsScript.cs
+++ Assets/CanvasFiles/MenuButtonsScript.cs
@@ -5,16 +5,12 @@ using Aryzon;
 public class MenuButtonsScript : AryzonRaycastInteractable
 {
     private GameObject menuCanvas;
-    private GameObject targetCanvas;
+    [SerializeField] private GameObject targetCanvas;
 
     protected override void Awake()
     {
         base.Awake();
         menuCanvas = GameObject.Find("CanvasMenu");
-        string name = "Canvas" + gameObject.name;
-        Debug.Log(name);
-        targetCanvas = GameObject.Find(name);
-        targetCanvas.SetActive(false);
     }
 
     protected override void Down()
