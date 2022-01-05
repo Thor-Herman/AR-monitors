b179c01f4426dd4263a8ca77295b6680910e9029
Thor-Herman van Eggelen <thorherman.eggelen@gmail.com>
2022-01-05 17:23:15 +0000

    Fix VideoStarte script after drive import

--- Assets/CanvasFiles/VideoStarte.cs
+++ Assets/CanvasFiles/VideoStarte.cs
@@ -2,17 +2,26 @@ using System.IO.Pipes;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.Video;
+using UnityEngine.UI;
 using Aryzon;
 
 public class VideoStarte : AryzonRaycastInteractable
 {
     private VideoPlayer vidPlayer;
+    public Sprite playBtn;
+    public Sprite stopBtn;
 
-    protected override void Awake()
+    protected override void OnEnable()
     {
-        base.Awake();
-        vidPlayer = gameObject.GetComponent<VideoPlayer>();
+        vidPlayer = gameObject.transform.parent.gameObject.GetComponent<VideoPlayer>();
         vidPlayer.Pause();
+        base.OnEnable();
+    }
+
+    protected override void OnDisable()
+    {
+        gameObject.GetComponent<Image>().sprite = playBtn;
+        base.OnDisable();
     }
 
     protected override void Down()
@@ -20,10 +29,20 @@ public class VideoStarte : AryzonRaycastInteractable
         if (vidPlayer.isPlaying)
         {
             vidPlayer.Pause();
+            gameObject.GetComponent<Image>().sprite = playBtn;
         }
         else
         {
             vidPlayer.Play();
+            gameObject.GetComponent<Image>().sprite = stopBtn;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (!vidPlayer.isPlaying)
+        {
+            gameObject.GetComponent<Image>().sprite = playBtn;
         }
     }
 }
