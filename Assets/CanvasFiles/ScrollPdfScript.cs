134624b17f1dbb1f1f18b88205cc3c633fa53d36
Thor-Herman van Eggelen <thorherman.eggelen@gmail.com>
2022-01-06 12:40:58 +0000

    Refactor remaining scripts

--- Assets/CanvasFiles/ScrollPdfScript.cs
+++ Assets/CanvasFiles/ScrollPdfScript.cs
@@ -1,114 +1,64 @@
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
+using Aryzon;
 
-namespace Aryzon
+public class ScrollPdfScript : AryzonRaycastInteractable
 {
-    // This class shows how to receive and handle reticle  events in your code.
-    // Place this component on an object with a renderer, like a cube and its
-    // material will change color when the reticle hovers over it.
-    // It is recommended to duplicate and edit this code in your own class.
 
-    //[RequireComponent(typeof(AryzonRaycastObject))]
-    public class ScrollPdfScript : MonoBehaviour { 
-
-        private AryzonRaycastObject raycastObject;
-        private GameObject pdfImage;
-        private GameObject currentScrollBar;
-        private bool pressedUp;
-        private bool isPressing;
-        private float y = 5.0f / (2898.0f + 2902.0f) * 284.1f;
+    private GameObject pdfImage;
+    private GameObject currentScrollBar;
+    private bool pressedUp;
+    private bool isPressing;
+    private float y = 5.0f / (2898.0f + 2902.0f) * 284.1f;
 
 
-        private void Awake()
+    protected override void Awake()
+    {
+        base.Awake();
+        pdfImage = GameObject.Find("PdfImage");
+        currentScrollBar = GameObject.Find("CurrentPositionScrollBar");
+    }
+
+    protected override void OnEnable()
+    {
+        pressedUp = gameObject.name.Equals("UpBtn");
+        //progressBar.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 0.0f);
+        base.OnEnable();
+    }
+
+    protected override void Off()
+    {
+        isPressing = false;
+    }
+
+    protected override void Over()
+    {
+        isPressing = true;
+
+    }
+    private void FixedUpdate()
+    {
+        if (isPressing)
         {
-            raycastObject = gameObject.GetComponent<AryzonRaycastObject>();
-            pdfImage = GameObject.Find("PdfImage");
-            currentScrollBar = GameObject.Find("CurrentPositionScrollBar");
-        }
-
-        private void OnEnable()
-        {
-            pressedUp = gameObject.name.Equals("UpBtn");
-            //progressBar.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 0.0f);
-            if (raycastObject)
+            if (pressedUp)
             {
-                raycastObject.OnPointerOff.AddListener(Off);
-                raycastObject.OnPointerOver.AddListener(Over);
-                raycastObject.OnPointerUp.AddListener(Clicked);
-                raycastObject.OnPointerDown.AddListener(Down);
-            }
-        }
-
-        private void OnDisable()
-        {
-   
-            if (raycastObject)
-            {
-                raycastObject.OnPointerOff.RemoveListener(Off);
-                raycastObject.OnPointerOver.RemoveListener(Over);
-                raycastObject.OnPointerUp.RemoveListener(Clicked);
-                raycastObject.OnPointerDown.RemoveListener(Down);
-            }
-        }
-
-        private void Off()
-        {
-            isPressing = false;
-        }
-
-        private void Over()
-        {
-            isPressing = true;
-           
-        }
-        private void FixedUpdate()
-        {
-            if (isPressing)
-            {
-                if (pressedUp)
+                if (pdfImage.GetComponent<RectTransform>().localPosition.y > -2898)
                 {
-                    if (pdfImage.GetComponent<RectTransform>().localPosition.y > -2898)
-                    {
-                        //pdfImage.GetComponent<RectTransform>().localPosition.y += 175;
-                        pdfImage.GetComponent<RectTransform>().localPosition -= new Vector3(0, 5, 0);
-                        currentScrollBar.GetComponent<RectTransform>().localPosition -= new Vector3(0, y, 0);
-                    }
-                }
-                else
-                {
-                    if (pdfImage.GetComponent<RectTransform>().localPosition.y < 2902)
-                    {
-                        //pdfImage.GetComponent<RectTransform>().localPosition.y -= 175;
-                        pdfImage.GetComponent<RectTransform>().localPosition += new Vector3(0, 5, 0);
-                        currentScrollBar.GetComponent<RectTransform>().localPosition -= new Vector3(0, y, 0);
-                    }
+                    //pdfImage.GetComponent<RectTransform>().localPosition.y += 175;
+                    pdfImage.GetComponent<RectTransform>().localPosition -= new Vector3(0, 5, 0);
+                    currentScrollBar.GetComponent<RectTransform>().localPosition -= new Vector3(0, y, 0);
                 }
             }
-        }
-        private void Down()
-        {
-        //    if (pressedUp)
-        //    {
-        //        if (pdfImage.GetComponent<RectTransform>().localPosition.y > -2898)
-        //        {
-        //            //pdfImage.GetComponent<RectTransform>().localPosition.y += 175;
-        //            pdfImage.GetComponent<RectTransform>().localPosition -= new Vector3(0, 175, 0);
-        //        }
-        //    }
-        //    else
-        //    {
-        //        if (pdfImage.GetComponent<RectTransform>().localPosition.y < 2902)
-        //        {
-        //            //pdfImage.GetComponent<RectTransform>().localPosition.y -= 175;
-        //            pdfImage.GetComponent<RectTransform>().localPosition += new Vector3(0, 175, 0);
-        //        }
-        //    }
-        }
-
-        private void Clicked()
-        {
-            //gameObject.GetComponent<VideoPlayer>().Stop();
+            else
+            {
+                if (pdfImage.GetComponent<RectTransform>().localPosition.y < 2902)
+                {
+                    //pdfImage.GetComponent<RectTransform>().localPosition.y -= 175;
+                    pdfImage.GetComponent<RectTransform>().localPosition += new Vector3(0, 5, 0);
+                    currentScrollBar.GetComponent<RectTransform>().localPosition -= new Vector3(0, y, 0);
+                }
+            }
         }
     }
 }
--- Assets/CanvasFiles/SelectFriendScript.cs
+++ Assets/CanvasFiles/SelectFriendScript.cs
@@ -3,84 +3,49 @@ using UnityEngine.Events;
 using System.Collections;
 using System.Collections.Generic;
 
-namespace Aryzon
+using Aryzon;
+public class SelectFriendScript : AryzonRaycastInteractable
 {
-    public class SelectFriendScript : MonoBehaviour
-    {
-        private AryzonRaycastObject raycastObject;
-        private GameObject[] profiles;
-        private bool isInitialized = false;
+    private GameObject[] profiles;
+    private bool isInitialized = false;
 
-        private void Awake()
+    protected override void Awake()
+    {
+        base.Awake();
+        profiles = GameObject.FindGameObjectsWithTag("FriendProfile");
+    }
+
+    protected override void OnEnable()
+    {
+        if (isInitialized)
         {
-            raycastObject = gameObject.GetComponent<AryzonRaycastObject>();
-            profiles =  GameObject.FindGameObjectsWithTag("FriendProfile");          
-        }
-        
-        private void OnEnable()
-        {
-            if (isInitialized)
+            for (int i = 0; i < 4; i++)
             {
-                for (int i = 0; i < 4; i++)
+                if (profiles[i].name.Equals("profile" + gameObject.name))
                 {
-                    if (profiles[i].name.Equals("profile" + gameObject.name))
-                    {
-                        profiles[i].SetActive(false);
-                    }
+                    profiles[i].SetActive(false);
                 }
+            }
+        }
+        else
+        {
+            isInitialized = true;
+        }
+        base.OnEnable();
+    }
+
+    protected override void Down()
+    {
+        foreach (GameObject profile in profiles)
+        {
+            if (profile.name.Equals("profile" + gameObject.name))
+            {
+                profile.SetActive(true);
             }
             else
             {
-                isInitialized = true;
+                profile.SetActive(false);
             }
-            if (raycastObject)
-            {
-                raycastObject.OnPointerOff.AddListener(Off);
-                raycastObject.OnPointerOver.AddListener(Over);
-                raycastObject.OnPointerUp.AddListener(Clicked);
-                raycastObject.OnPointerDown.AddListener(Down);
-            }
-        }
-
-        private void OnDisable()
-        {
-            if (raycastObject)
-            {
-                raycastObject.OnPointerOff.RemoveListener(Off);
-                raycastObject.OnPointerOver.RemoveListener(Over);
-                raycastObject.OnPointerUp.RemoveListener(Clicked);
-                raycastObject.OnPointerDown.RemoveListener(Down);
-            }
-        }
-
-        private void Off()
-        {
-
-        }
-
-        private void Over()
-        {
-
-        }
-
-        private void Down()
-        {
-            foreach( GameObject profile in profiles)
-            {
-                if(profile.name.Equals("profile" + gameObject.name))
-                {
-                    profile.SetActive(true);
-                }
-                else
-                {
-                    profile.SetActive(false);
-                }
-                
-            }
-        }
-
-        private void Clicked()
-        {
 
         }
     }
