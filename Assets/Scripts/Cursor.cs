0c61cac3c601165cbf1e6bf49fc82253bacec4b5
Thor-Herman van Eggelen <44784559+Thor-Herman@users.noreply.github.com>
2022-02-01 13:58:42 +0000

    Fix monitor border with transparency slider

--- Assets/Scripts/Cursor.cs
+++ Assets/Scripts/Cursor.cs
@@ -23,7 +23,7 @@ public class Cursor : MonoBehaviour
     void Start()
     {
         Input.simulateMouseWithTouches = false;
-        UnityEngine.Cursor.lockState = CursorLockMode.Locked;
+        // UnityEngine.Cursor.lockState = CursorLockMode.Locked;
         rectTransform = GetComponent<RectTransform>();
         upTrigger.enabled = false;
         downTrigger.enabled = false;
@@ -134,7 +134,6 @@ public class Cursor : MonoBehaviour
 
     private void ToggleMonitorBorder(RectTransform toggleScreen, bool enabled)
     {
-        Transform border = toggleScreen.Find("Border");
-        border.gameObject.GetComponentInChildren<UnityEngine.UI.Image>().enabled = enabled;
+        toggleScreen.Find("Border").gameObject.SetActive(enabled);
     }
 }
